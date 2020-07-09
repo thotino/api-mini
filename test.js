b5233ca370c3f2e3e6e86da2c3f5aa5455acf344
Thotino GOBIN-GANSOU <gthomario@yahoo.com>
2020-07-09 16:51:03 +0200

    feat: improved test script

--- test.js
+++ test.js
@@ -12,11 +12,26 @@ const user = {
     lastName: "GOBIN-GANSOU",
     mailAdress: "gthomario@yahoo.com"
 };
+/*
+{
+  _id: '5f05fc3b11806d3c663943bb',
+  firstName: 'Thotino',
+  lastName: 'GOBIN-GANSOU',
+  mailAdress: 'gthomario@yahoo.com',
+  registrationDate: '2020-07-08T17:02:51.952Z',
+  createdAt: '2020-07-08T17:02:51.956Z',
+  updatedAt: '2020-07-08T17:02:51.956Z',
+  __v: 0
+}
+*/
+const order = {
+    userId: '5f05fc3b11806d3c663943bb',
+    figuresQuantity: 5,
+};
 
 return Promise.try(() => {
     return request.postAsync({
         uri: "http://127.0.0.1:1200/user",
-        method: "POST",
         json: true,
         headers: {
             "Accept": "application/json",
@@ -25,5 +40,25 @@ return Promise.try(() => {
         body: user,
     }).then((result) => {
         console.log(result[1]);
-    })
+        return result[1];
+        
+    }).then((userCreated) => {
+        const orderToCreate = {
+            userId: userCreated._id,
+            figuresQuantity: 5,
+        };
+        console.log(orderToCreate);
+        return request.postAsync({
+            uri: "http://127.0.0.1:1200/order",
+            json: true,
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            body: orderToCreate,
+        })
+    }).then((result) => {
+        console.log(result[1]);
+        return result[1];
+    });
 })
