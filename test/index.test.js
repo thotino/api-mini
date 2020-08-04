ea9f91acd466504e99924e2de8693a3bb64dd8eb
Thotino GOBIN-GANSOU <gthomario@yahoo.com>
2020-08-04 12:59:12 +0200

    feat: test completed

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const Promise = require("bluebird");
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 const request = Promise.promisifyAll(require("request").defaults({jar: true}), {
   filter: (funcName) => { return /put|patch|post|head|del(ete)?|get/.test(funcName); },
   multiArgs: true,
@@ -11,6 +11,10 @@ const user = {
     firstName: "John",
     lastName: "Doe",
     mailAdress: "john.doe@domain.com"
+};
+
+const order = {
+    figuresQuantity: 1,
 };
 
 const createUser = function() {
@@ -23,13 +27,28 @@ const createUser = function() {
         },
         body: user, 
     }).then((result) => {
-        return result[1];
+        return result;
+    });
+};
+
+const createOrder = function() {
+    return request.postAsync({
+        uri: "http://127.0.0.1:1200/order",
+        json: true,
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        body: order, 
+    }).then((result) => {
+        return result;
     });
 };
 
 describe("POST /user", function() {
     it("Should create a new user", function() {
-        return createUser().then((data) => {
+        return createUser().then(([response, data]) => {
+            expect(response.statusCode).to.equal(200);
             assert.isObject(data);
             assert.isNotEmpty(data);
             assert.hasAllKeys(data, ["_id", "firstName", "lastName", "mailAdress", "registrationDate", "createdAt", "updatedAt", "__v"]);
@@ -37,6 +56,19 @@ describe("POST /user", function() {
             assert.strictEqual(data.lastName, user.lastName);
             assert.strictEqual(data.mailAdress, user.mailAdress);
 
+        });
+    });
+});
+
+describe("POST /order", function() {
+    it("Should not create a new order", function() {
+        return createOrder().then(([response, data]) => {
+            expect(response.statusCode).to.equal(500);
+            assert.isObject(data);
+            assert.isNotEmpty(data);
+            assert.hasAllKeys(data, ["code", "message"]);
+            assert.strictEqual(data.code, "InternalServer");
+            assert.strictEqual(data.message, "internal server error");
         });
     });
 });
